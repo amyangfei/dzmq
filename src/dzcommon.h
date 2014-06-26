62e8ac1a6822985374b718d098a5c8c294ce9619
amyangfei <amyangfei@gmail.com>
2014-06-26 18:29:12 +0800

    Minor update of macros

--- src/dzcommon.h
+++ src/dzcommon.h
@@ -8,6 +8,19 @@
 #include <stdbool.h>
 #include "czmq.h"
 
+#ifdef __APPLE__
+#include <AvailabilityMacros.h>
+#endif
+
+/* For polling API */
+#ifdef __linux__
+#define HAVE_EPOLL 1
+#endif
+
+#if (defined(__APPLE__) && defined(MAC_OS_X_VERSION_10_6)) || defined(__FreeBSD__) || defined(__OpenBSD__) || defined (__NetBSD__)
+#define HAVE_KQUEUE 1
+#endif
+
 #define NOTUSED(V) ((void) V)
 #define MAX_LINE 1024
 
